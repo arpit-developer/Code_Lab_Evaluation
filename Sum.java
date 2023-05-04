f05918ca059af6fcf52d3beddf76cf0a4fad3599
arpit-developer <arpit.bhatnagar55@gmail.com>
2023-05-05 03:49:11 +0530

    lastCommmit

--- Sum.java
+++ Sum.java
@@ -7,5 +7,6 @@ public static void main(String[] args){
 	System.out.println("Sum of a & b is: "+c);
 	System.out.println(x);
 	System.out.println("This is change for build in jenkins");
+	System.out.println("This is change for build in jenkins through local.");
 	}
 }
